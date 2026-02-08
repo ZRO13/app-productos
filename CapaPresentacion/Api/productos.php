247c5c3572c86a29d45ed0eb1f9e272a6b52001b
Zero13Dev <juliocoloma1875@gmail.com>
2026-02-08 10:51:18 -0500

    x

--- CapaPresentacion/Api/productos.php
+++ CapaPresentacion/Api/productos.php
@@ -3,11 +3,6 @@ require_once __DIR__.'/../../CapaNegocio/ProductoNegocio.php';
 
 session_start();
 
-// CORS
-header("Access-Control-Allow-Origin: http://localhost:5173");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Content-Type: application/json");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
